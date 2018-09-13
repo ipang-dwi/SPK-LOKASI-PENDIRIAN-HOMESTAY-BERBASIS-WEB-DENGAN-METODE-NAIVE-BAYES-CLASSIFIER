87e1853b2c262ce149feef7ade4b8b975a5bde77
ipang dwi <admin@firstplato.com>
2018-09-13 14:41:57 +0700

    Update configdb.php

--- configdb.php
+++ configdb.php
@@ -3,7 +3,7 @@
 	$_SESSION['judul'] = 'SPK NBC HOMESTAY';
 	$_SESSION['welcome'] = 'SISTEM PENGAMBIL KEPUTUSAN LOKASI PENDIRIAN HOMESTAY BERBASIS WEB DENGAN METODE NAIVE BAYES CLASSIFIER';
 	$_SESSION['by'] = 'ShouldBe';
-	$mysqli = new mysqli('localhost','root','1717','bayes');
+	$mysqli = new mysqli('localhost','root','','bayes');
 	if($mysqli->connect_errno){
 		echo $mysqli->connect_errno." - ".$mysqli->connect_error;
 		exit();
